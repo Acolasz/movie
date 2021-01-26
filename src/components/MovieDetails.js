839c4aacf34942dade98b558caf5efc652bbc531
Olivér <oliver.ocsko@dorsum.eu>
2021-01-26 20:51:34 +0100

    Icons display, onMouse event, renderer solution problem and key with const

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 function MovieDetails(props) {
 
+  const [ highlighted, setHighlighted ] = useState(-1);
+  const mov = props.movie;
+  const highlightRate = high => evt => {
+    setHighlighted(high)
+  };
+
   return (
-    <div>
+    <React.Fragment>
       {
-        props.movie ? (
+        mov ? (
           <div>
-            <h1>{props.movie.name}</h1>
-            <p>{props.movie.description}</p>
-            <p>{props.movie.detail1}</p>
-            <p>{props.movie.detail2}</p>
+            <h1>{mov.name}</h1>
+            <p>{mov.description}</p>
+            <p>{mov.detail1}</p>
+            <p>{mov.detail2}</p>
+            <FontAwesomeIcon icon={faStar} className='orange'/>
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+            <div className="rate-container">
+              <h2>Rate it</h2>
+              { [...Array(5)].map( (e, i) => {
+                return <FontAwesomeIcon key={i} icon={faStar} className={highlighted > i - 1 ? 'purple' : ''}
+                                        onMouseEnter={highlightRate(i)}
+                                        onMouseLeave={highlightRate(-1)}
+                />
+              })}
+            </div>
           </div>
         ) : null
       }
-    </div>
+    </React.Fragment>
   )
 }
 
